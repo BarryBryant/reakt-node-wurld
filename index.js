1a58b0d89b39580adffa40f8c5bc3fa16a9db200
Barry Bryant <barry.bryant@willowtreeapps.com>
2018-01-31 16:29:40 -0500

    chore: test deploy update
    feat: OAUTH Setup

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// 621338310528-9vk38sk8hp17lkbn72p99btkd1005dnu.apps.googleusercontent.com client id
+// IQZoahGdLXgzfBpCVCOvxkFi client secret
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express(); // generates a new application
 
-app.get("/", (req, res) => {
-	res.send({ hi: "there" });
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

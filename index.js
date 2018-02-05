bf2533ab84c746e19da369ab64e91eb8d4d6e5cc
Barry Bryant <barry.bryant@willowtreeapps.com>
2018-02-05 14:24:05 -0500

    feat: Mongo setup

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 require("./services/passport");
+require("./models/user");
 
 const app = express(); // generates a new application
+
+mongoose.connect(keys.mongoURI);
 
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
